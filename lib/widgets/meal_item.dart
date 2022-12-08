5806afc0f4027e246ea166ce2a73830aca3a18a3
gem00n <o.ghanaiem@student.aaup.edu>
2022-12-08 14:01:25 +0200

    170. Finishing the Meal List Item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -14,6 +14,47 @@ class MealItem extends StatelessWidget {
       @required this.duration,
       @required this.affordability,
       @required this.complexity});
+  String get ComplexityText {
+    // if (complexity == Complexity.Simple) {
+    //   return 'Simple';
+    // }
+    // if (complexity == Complexity.Challenging) {
+    //   return '...';
+    // }
+    // if (complexity == Complexity.Hard) {
+    //   return '...';
+    // }
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get AffordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+      default:
+        return 'Affordable';
+        break;
+    }
+  }
 
   void selectMeal() {}
 
@@ -25,7 +66,7 @@ class MealItem extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
         ),
-        elevation: 4,
+        elevation: 10,
         margin: EdgeInsets.all(10),
         child: Column(
           children: <Widget>[
@@ -33,8 +74,8 @@ class MealItem extends StatelessWidget {
               children: <Widget>[
                 ClipRRect(
                   borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15)),
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
                   child: Image.network(
                     imageUrl,
                     height: 250,
@@ -42,8 +83,73 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 28, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
             ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.schedule,
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        '$duration min',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.work,
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        ComplexityText,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.attach_money,
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        AffordabilityText,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
